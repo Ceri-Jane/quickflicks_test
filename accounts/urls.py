3a3f204d4977fb06060545c5a953da2627c3d382
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-05 17:27:11 +0000

    Fix logout: use POST form instead of GET link to avoid 405 (Django LogoutView requires POST)

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,14 +5,11 @@ from . import views
 urlpatterns = [
     path("profile/", views.profile, name="profile"),
 
-    # Login uses our moved template
     path("login/", auth_views.LoginView.as_view(
         template_name="registration/login.html"
     ), name="login"),
 
-    # Logout and bounce to home (we already set LOGOUT_REDIRECT_URL = "/")
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
 
-    # Signup (wired in next step)
     path("signup/", views.signup, name="signup"),
 ]
