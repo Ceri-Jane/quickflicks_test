aef145f5491b1452b26ba176bfa832c7c960b97b
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-05 15:35:56 +0000

    Add custom login and signup system

--- quickflicks/urls.py
+++ quickflicks/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('movies.urls')),  # <--- add this
+    path('', include('movies.urls')),
+    path('accounts/', include('users.urls')),
 ]
