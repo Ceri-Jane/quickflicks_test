2e56c8320ebe94f42f9106b4c9cae6231fd079a1
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-05 17:31:38 +0000

    Add Movie model to store user’s saved films with watched status

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Movie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tmdb_id = models.CharField(max_length=50)
+    title = models.CharField(max_length=255)
+    poster_url = models.URLField(max_length=500, blank=True, null=True)
+    watched = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({'Watched' if self.watched else 'Not Watched'})"
