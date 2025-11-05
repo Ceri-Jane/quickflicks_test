1c195f301370539b7fc4018b3e99fec005d5dbaa
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-05 17:09:43 +0000

    feat(auth): add signup view using UserCreationForm with custom template

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 @login_required
 def profile(request):
     return render(request, "accounts/profile.html")
 
-
-class CustomLoginView(LoginView):
-    template_name = "accounts/login.html"
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created! Please log in.")
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/signup.html", {"form": form})
