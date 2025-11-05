ea15a3d3aa6074db7f488b3a478c73881eb06782
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-05 15:14:13 +0000

    Add TMDb movie search functionality

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import requests
+from django.conf import settings
 
 def home(request):
     query = request.GET.get('query', '')
-    return render(request, 'movies/home.html', {'query': query})
+    movies = []
+
+    if query:
+        api_key = settings.TMDB_API_KEY
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={query}"
+        response = requests.get(url).json()
+        movies = response.get('results', [])
+
+    return render(request, 'movies/home.html', {
+        'query': query,
+        'movies': movies,
+    })
