59bd0fd7624a58c208fceb7971feec5ec37d3a1b
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-12 21:57:36 +0000

    commit

--- quickflicks/settings.py
+++ quickflicks/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-kks*9x7rwgjbxw7ggban+vz)jyoezzw1^^26itg(l8fyjq*$#g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*',]
 
 
 # Application definition
